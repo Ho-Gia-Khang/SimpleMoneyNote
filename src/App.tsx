baeab31cce117586b9fccdb751efa90840850aa8
Gia Khang <hogiakhanglatoi@gmail.com>
2024-02-28 11:10:17 +0700

    add custom request sending functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Get } from "./api/Requests";
+import { TodoProps } from "./types";
 
 function App() {
+    const [data, setData] = React.useState<TodoProps>();
+    const getData = async () => {
+        const newData = await Get<TodoProps>("todos/1");
+        setData(newData);
+        console.log(newData);
+        console.log(data);
+    };
+
     return (
         <main>
-            <h1 className="bg-blue-400">Hello, World!</h1>
+            <button onClick={getData}>Get Data</button>
         </main>
     );
 }
---
+++ src/api/Requests.tsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+let data: any = {};
+
+async function fetchData<T>(extendURL: string) {
+    await axios
+        .get(`${API_URL}${extendURL}`)
+        .then((response) => {
+            if (response.status === 200) {
+                console.log(response.data);
+                data = response.data as T;
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            data = null;
+        });
+}
+
+export async function Get<T>(extendURL: string): Promise<T | undefined> {
+    await fetchData<T>(extendURL);
+    if (data) {
+        return data as T;
+    }
+    return undefined;
+}
+
+export async function Post<T>(
+    extendURL: string,
+    data: T
+): Promise<boolean | undefined> {
+    try {
+        await axios.post(`${API_URL}${extendURL}`, data).then((response) => {
+            if (response.status === 201) {
+                console.log("Post successful");
+                return true;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export async function Put<T>(
+    extendURL: string,
+    data: T
+): Promise<Boolean | undefined> {
+    try {
+        await axios.put(`${API_URL}${extendURL}`, data).then((response) => {
+            if (response.status === 200) {
+                console.log("Put successful");
+                return true;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export async function Delete(extendURL: string): Promise<Boolean | undefined> {
+    try {
+        await axios.delete(`${API_URL}${extendURL}`).then((response) => {
+            if (response.status === 200) {
+                console.log("Delete successful");
+                return true;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
