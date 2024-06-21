5e656bf780aba90f916a78201ef3157f86aebbef
Gia Khang <hogiakhanglatoi@gmail.com>
2024-06-21 21:40:41 +0700

    sprint 5 | add create wallet

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -2,10 +2,11 @@ import React, { useMemo } from "react";
 import SinglePage from "src/components/ui/layout/SinglePage";
 import { useLogin } from "src/stores/LoginStore";
 import LoginReminder from "src/components/ui/layout/LoginReminder";
-import { useWalletInfo } from "src/stores/WalletStore";
+import { useWallet, useWalletInfo } from "src/stores/WalletStore";
 import SingleWallet from "src/components/functional/SingleWallet";
 import WalletHeader from "src/components/functional/WalletHeader";
 import ColoredSeparator from "src/components/ui/coloredSeparator";
+import { Button } from "src/components/ui/button";
 
 const Wallet = () => {
     const allWalletInfos = useWalletInfo((state) => state.walletInfos);
@@ -33,6 +34,20 @@ const Wallet = () => {
                                 walletId={walletInfo.id}
                             />
                         ))}
+                        <Button
+                            variant={"outline"}
+                            className="absolute bg-transparent border border-black rounded-full bottom-2 right-2 h-fit"
+                            onClick={() => {
+                                useWallet.getState().setIsCreatingWallet(true);
+                            }}
+                        >
+                            <img
+                                src="icons/note/add-icon.jpg"
+                                alt="add-icon"
+                                width={42}
+                                height={42}
+                            />
+                        </Button>
                     </div>
                 ) : (
                     <></>
